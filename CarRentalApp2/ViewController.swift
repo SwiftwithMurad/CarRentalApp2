58999191c0ca30c83ea5466842a6e7d14c59b2c4
Murad Abbaszada <abbaszademurad3@gmail.com>
2024-12-27 10:59:32 +0400

    first commit

---
+++ CarRentalApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRentalApp2
+//
+//  Created by Mac on 27.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
